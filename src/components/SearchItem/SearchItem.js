9b2f9bde0a69f0ffbf1ffda450b9fd0eb2219d4f
Andressa Melo <contato@andressamelo.com.br>
2020-06-23 15:01:27 -0300

    add free shipping icon

--- src/components/SearchItem/SearchItem.js
+++ src/components/SearchItem/SearchItem.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './SearchItem.scss';
+import shipping from '../../assets/ic_shipping.png';
+import shipping2x from '../../assets/ic_shipping@2x.png';
 import NumberFormat from 'react-number-format';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const SearchItem = props => (
     <Link to={`/item/${props.id}`} className='SearchItem flex__strech'>
@@ -10,8 +12,12 @@ const SearchItem = props => (
             <div></div>
         </div>
         <div className='SearchItem__price--title'>
-            <h2><NumberFormat value={props.price} displayType={'text'} thousandSeparator={true} prefix={'$'} /></h2>
-            <h3>{props.free_shipping}</h3>
+            <h2>
+                <NumberFormat value={props.price} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+                {props.shipping.free_shipping === true &&
+                    <img src={shipping} srcSet={shipping + ' 1x,' + shipping2x + ' 2x'} alt='Envio Grátis' className='SearchItem__price--title--icon' />
+                }
+            </h2>
             <h3>{props.title}</h3>
         </div>
         <p className='SearchItem__state--name'>{props.address.state_name}</p>
